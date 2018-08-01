e034ae7176a88f2422060a3c35c82d0780408e1d
golliet <golliet@student.42.fr>
2018-08-01 16:26:34 +0200

    renforcer parsing, faire les bords

--- srcs/parse.c
+++ srcs/parse.c
@@ -44,11 +44,13 @@ static int		ft_add_list(char *str, t_list **list, int y)
 		else
 		{
 			nb = ft_atoi(str + i);
+			printf("%d ", nb);
 			ft_add(x, y, nb, list);
 			i += ft_rank(nb);
 			x++;
 		}
 	}
+	printf("true x : %d \n", x);
 	return (x);
 }
 
@@ -68,17 +70,15 @@ static void		ft_open(char *str, t_list **list, t_parse *parse)
 	}
 	while (get_next_line(fd, &lin) > 0)
 	{
-		/*if (!ft_lin_is_good(lin))
+		if (!ft_lin_is_good(lin))
 		{
 			free(lin);
 			close(fd);
 			// free la liste
 			ft_putstr("fsdfsdf\n");
 			exit(0);
-		}*/
+		}
 		x = ft_add_list(lin, list, y);
-		printf("%d ", x);
-		printf("%s\n", lin);
 		if (x > parse->nb_elem_line)
 			parse->nb_elem_line = x;
 		free(lin);
